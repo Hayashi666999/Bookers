b19b4db7c67b79af3cf257a7661bd420767dfdbc
EC2 Default User <ec2-user@ip-172-31-9-36.us-west-1.compute.internal>
2022-06-27 15:30:43 +0000

    アプリケーション課題６

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,19 +3,41 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books = Books.all  
+    @books = Book.all
+    @book = Book.new
   end
 
    def show
-    @books = Books.find(params[:id])  
+    @books = Book.find(params[:id])  
   end
 
    def edit
-    @books = Books.find(params[:id])
+    @books = Book.find(params[:id])
+  end
+  
+   def update
+    books = Book.find(params[:id])
+    books.update(books_params)
+    redirect_to books_path(books.id)  
+  end
+  
+  def destroy
+    books = Book.find(params[:id])
+    boo.destroy 
+    redirect_to '/books'
+  end
+  
+  def create
+    @books = Book.new(books_params)
+    if @books.save
+      redirect_to @books, flash: { notice: '記事の作成が作成されました。' }
+    else
+      redirect_to new_books_@path
+    end
   end
   
   private
   def books_params
-    params.require(:books).permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/edit'
-  
-  post 'books' => 'books#create'   #ここを追記します
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show'
+
   get '/top' => 'homes#top'
  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
