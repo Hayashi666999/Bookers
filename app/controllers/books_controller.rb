3ce3418e05ed6f65aec0b3cf79a7b583a83921dc
EC2 Default User <ec2-user@ip-172-31-9-36.us-west-1.compute.internal>
2022-06-30 15:32:36 +0000

    アプリケーションを作成しよう課題７

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,35 +7,38 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
-   def show
-    @books = Book.find(params[:id])  
-  end
-
-   def edit
+  def show
     @books = Book.find(params[:id])
   end
-  
-   def update
+
+  def edit
+    @books = Book.find(params[:id])
+  end
+
+  def update
     books = Book.find(params[:id])
     books.update(books_params)
-    redirect_to books_path(books.id)  
+    redirect_to book_path(books.id)
+    flash[:notice] = "Book was successfully update."
+
   end
-  
+
   def destroy
-    books = Book.find(params[:id])
-    boo.destroy 
+    book = Book.find(params[:id])
+    book.destroy
     redirect_to '/books'
   end
-  
+
   def create
-    @books = Book.new(books_params)
-    if @books.save
-      redirect_to @books, flash: { notice: '記事の作成が作成されました。' }
+    @book = Book.new(books_params)
+    if @book.save
+      redirect_to book_path(@book.id), flash: { notice: '記事の作成が作成されました。' }
     else
-      redirect_to new_books_@path
+    @books = Book.all
+      render :index
     end
   end
-  
+
   private
   def books_params
     params.require(:book).permit(:title, :body)
